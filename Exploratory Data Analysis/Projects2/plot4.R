d91feab57a0700984ff1e78c2d0c564315cacf8d
zentha <delphine.imbert@efrei.net>
2023-07-08 12:16:24 +0200

    Create plot4.R

---
+++ Exploratory Data Analysis/Projects2/plot4.R
@@ -0,0 +1,7 @@
+library("data.table")
+library("ggplot2")
+
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
